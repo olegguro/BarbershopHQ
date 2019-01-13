26393367adae739b4ce37644a523bc5daf75ddaa
Oleg Guro <ooleg6334@gmail.com>
2019-01-13 23:53:46 +0300

    Save information the lame way

--- app.rb
+++ app.rb
@@ -28,6 +28,22 @@ end
 
 post '/visit' do
 
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# name, phone, datestamp, barber, color
+	#сохранение данных
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datatime
+	c.barber = @barber
+	c.color = @color
+	c.save 
+
 		erb "<h2>Спасибо, вы записались!</h2>"
 	
 end
