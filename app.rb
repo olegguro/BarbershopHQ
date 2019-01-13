587473cb3740afa4655c053e5e658e4e915b8e79
Oleg Guro <ooleg6334@gmail.com>
2019-01-13 19:21:02 +0300

    Add Client entity

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#подключение к БД
+set :database, "sqlite3:barbershop.db"
+
+class Client < activeRecord::Base
+end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello!"			
 end
